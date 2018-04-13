411a8769fa7ce11ffc455dcf9cea74ff178c104d
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2018-04-13 01:23:31 -0400

    the most stressful following of directions ever

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+// const $ = require('jquery');
+//
+let num = 0;
+
+// got how to use setInterval off of w3schools
+// got use of ` vs. ' off of stack overflow
+setInterval(
+  () => {
+    $('#main').html(`youve been on this page for ${num} seconds`);
+    num += 1;
+  },
+  1000,
+);
